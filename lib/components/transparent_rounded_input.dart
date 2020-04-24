ccc8226e7ffd7da0d8afe922742d1d5b8c7e9f5a
Paul Musat <paul.musat@mail.concordia.ca>
2020-04-24 15:10:14 -0400

    Started UI for login screen

---
+++ lib/components/transparent_rounded_input.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TransparentRoundedInput extends StatelessWidget {
+  TransparentRoundedInput({this.hintText, this.obscureText = false});
+
+  final String hintText;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: TextField(
+        onChanged: (value) {
+          //Do something with the user input.
+        },
+        obscureText: this.obscureText,
+        decoration: InputDecoration(
+          hintText: this.hintText,
+          fillColor: Colors.white24,
+          filled: true,
+          contentPadding:
+          EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white54, width: 0),
+            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white54, width: 0),
+            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:eventify/components/rounded_button.dart';
+import 'package:eventify/components/transparent_rounded_input.dart';
+import 'package:eventify/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,8 +14,73 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            // Background gradient
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF6AF3C5),
+                  Color(0xFF3D77F8),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'EVENTIFY',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 50.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TransparentRoundedInput(hintText: 'Enter your email'),
+                TransparentRoundedInput(
+                    hintText: 'Enter your password', obscureText: true),
+                RoundedButton(
+                  title: 'LOG IN',
+                  onPressed: () {
+                    //Implement registration functionality.
+                  },
+                  backgroundColor: Color(0xFFF7FFF6),
+                  textColor: Color(0xFF3D77F8),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Dont't have an account?"),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, RegistrationScreen.id);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Text("Register", style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
